6a22f4603ec79a2790a66aff7894e926b12e48e3
tosinjones <benjamin.o.dada@gmail.com>
2015-06-26 10:18:12 +0000

    finish the toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: "I Love you ({})"
+    render text: "I Love you!!!"
   end
 end
